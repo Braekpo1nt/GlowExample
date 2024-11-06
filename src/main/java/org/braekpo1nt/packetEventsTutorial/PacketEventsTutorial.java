72933835b30e0beed73819a132415794592b0697
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-11-06 14:23:14 -0500

    added first attempt

--- src/main/java/org/braekpo1nt/packetEventsTutorial/PacketEventsTutorial.java
+++ src/main/java/org/braekpo1nt/packetEventsTutorial/PacketEventsTutorial.java
@@ -1,17 +1,24 @@
 package org.braekpo1nt.packetEventsTutorial;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PacketEventsTutorial extends JavaPlugin {
     
     @Override
+    public void onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+    }
+    
+    @Override
     public void onEnable() {
-        // Plugin startup logic
-        
+        PacketEvents.getAPI().init();
     }
     
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        PacketEvents.getAPI().terminate();
     }
 }
