a70cff7af5b5eff8792ba3f6ec51907fbf418207
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-11-06 13:27:18 -0500

    initial commit

---
+++ src/main/java/org/braekpo1nt/packetEventsTutorial/PacketEventsTutorial.java
@@ -0,0 +1,17 @@
+package org.braekpo1nt.packetEventsTutorial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PacketEventsTutorial extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
